b2572ded72f66adce7f6f96390748b2d895363ba
Sohail Ishaque <sohail.sohailishaq@gmail.com>
2022-08-18 05:57:40 +0500

    changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 
+const { PRIVATE_KEY, API_URL_BSC, PRIVATE_KEY_polygon, YOUR_ETHERSCAN_API_KEY, API_URL_ropsten,  API_URL_mumbai } = process.env;
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
@@ -8,12 +10,29 @@ module.exports = {
   networks: {
     aurora: {
       url: `https://mainnet.aurora.dev`,
-      accounts: [process.env.privateKey],
+      accounts: [`0x${PRIVATE_KEY}`],
     },
     fantom: {
       url: `https://rpc.ftm.tools/`,
-      accounts: [process.env.privateKey],
+      accounts: [`0x${PRIVATE_KEY}`],
     },
+    ropsten: {
+      url: API_URL_ropsten,
+      accounts: [`0x${PRIVATE_KEY}`]
+    },
+    bsc: {
+      url: API_URL_BSC,
+      accounts: [`0x${PRIVATE_KEY}`]
+    },
+    mumbai: {
+      url: API_URL_mumbai,
+      accounts: [`0x${PRIVATE_KEY_polygon}`]
+    }
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: YOUR_ETHERSCAN_API_KEY
   },
   solidity: {
     compilers: [
